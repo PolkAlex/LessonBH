55f6bb195befad59dd599e7d40c18d273d53c880
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-05-14 18:48:02 +0300

    add javatask1

---
+++ src/lesson02/JavaTask2.java
@@ -0,0 +1,43 @@
+package lesson02;
+
+import java.util.Scanner;
+
+public class JavaTask2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        step1();
+    }
+
+    static void step1() {
+        // слева направо
+        int n = 5; // Количество чисел в строке и в столбце
+        int maxValue = 25; // Максимальное значение числа
+
+//        for (int i = 1; i <= maxValue; i++) {
+//            System.out.print(i + " ");
+//            if (i % n == 0) {
+//                System.out.println();
+//            }
+//        }
+        // сверху вниз ??????????????????
+//        for (int i = 1; i <= maxValue; i++) {
+//            if (i == 1 || i % n == 1) {
+//                System.out.println(i + " ");
+//            }
+//            if (i == 2 || i % n == 2) {
+//                System.out.println(i + " ");
+//            }
+//            if (i == 3 || i % n == 3) {
+//                System.out.println(i + " ");
+//            }
+//            if (i == 4 || i % n == 4) {
+//                System.out.println(i + " ");
+//            }
+//            if (i == 5 || i % n == 5) {
+//                System.out.println(i + " ");
+//            }
+//        }
+    }
+}
