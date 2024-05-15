162a0a734cd164365a358e8d779fc8d97d61450f
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-05-15 10:27:52 +0300

    optimize code

--- src/lesson02/JavaTask1.java
+++ src/lesson02/JavaTask1.java
@@ -1,8 +1,5 @@
 package lesson02;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class JavaTask1 {
@@ -16,45 +13,52 @@ public class JavaTask1 {
             int i1 = Integer.parseInt(s);
             array[i] = i1;
         }
-//        step1(array);
-//        step2(array);
+        step1(array);
+        step2(array);
         step3(array);
     }
 
     // --------- найти минимальное и максимальное значение в массиве ---------
-//        static void step1(int[] array) {
-//
-//        int max = array[0];
-//        int minList = array[0];
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] > max) {
-//                max = array[i];
-//            }
-//            if (array[i] < minList) {
-//                minList = array[i];
-//            }
-//        }
-//        System.out.println("max = " + max);
-//        System.out.println("minList = " + minList);
-//    }
+        static void step1(int[] array) {
+
+        System.out.println("--найти минимальное и максимальное значение в массиве--");
+
+        int max = array[0];
+        int minList = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < minList) {
+                minList = array[i];
+            }
+        }
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + minList);
+    }
     // --------- найти числа, которые меньше среднего арифметического в массиве ---------
-//    static void step2(int[] array) {
-//
-//        int sum = 0;
-//        for (int i = 0; i < array.length; i++) {
-//            sum = sum + i;
-//        }
-//        double average = (double) sum / array.length;
-//        System.out.println("Average of array = " + average);
-//        System.out.print("minList of average: ");
-//        for (int i = 0; i < array.length; i++) {
-//            if (i < average) {
-//                System.out.print(array[i] + "; ");
-//            }
-//        }
-//    }
+    static void step2(int[] array) {
+
+        System.out.println("--найти числа, которые меньше среднего арифметического в массиве--");
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + i;
+        }
+        double average = (double) sum / array.length;
+        System.out.println("Average of array = " + average);
+        System.out.print("Min of average: ");
+        for (int i = 0; i < array.length; i++) {
+            if (i < average) {
+                System.out.print(array[i] + "; ");
+            }
+        }
+        System.out.println();
+    }
     // --------- найти номера найменьших чисел в массиве в порядке убывания ---------
     static void step3(int[] array) {
+
+        System.out.println("--найти номера найменьших чисел в массиве в порядке убывания--");
 
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -62,8 +66,7 @@ public class JavaTask1 {
                 min = i;
             }
         }
-
-        System.out.print("min index of array = ");
+        System.out.print("Min index of array = ");
         boolean first = true;
         for (int j = 0; j < array.length; j++) {
             if (array[j] == min) {
--- src/lesson02/JavaTask2.java
+++ src/lesson02/JavaTask2.java
@@ -8,36 +8,29 @@ public class JavaTask2 {
 
         Scanner sc = new Scanner(System.in);
         step1();
+        System.out.println("----------//----------");
     }
 
     static void step1() {
         // слева направо
+        System.out.println("слева направо: ");
         int n = 5; // Количество чисел в строке и в столбце
         int maxValue = 25; // Максимальное значение числа
 
-//        for (int i = 1; i <= maxValue; i++) {
-//            System.out.print(i + " ");
-//            if (i % n == 0) {
-//                System.out.println();
-//            }
-//        }
-        // сверху вниз ??????????????????
-//        for (int i = 1; i <= maxValue; i++) {
-//            if (i == 1 || i % n == 1) {
-//                System.out.println(i + " ");
-//            }
-//            if (i == 2 || i % n == 2) {
-//                System.out.println(i + " ");
-//            }
-//            if (i == 3 || i % n == 3) {
-//                System.out.println(i + " ");
-//            }
-//            if (i == 4 || i % n == 4) {
-//                System.out.println(i + " ");
-//            }
-//            if (i == 5 || i % n == 5) {
-//                System.out.println(i + " ");
-//            }
-//        }
+        for (int i = 1; i <= maxValue; i++) {
+            System.out.print(i + " ");
+            if (i % n == 0) {
+                System.out.println();
+            }
+        }
+        // сверху вниз
+        System.out.println("сверху вниз: ");
+        int[][] array = {{1, 6, 11, 16, 21}, {2, 7, 12, 17, 22}, {3, 8, 13, 18, 23}, {4, 9, 14, 19, 24}, {5, 10, 15, 20, 25}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
