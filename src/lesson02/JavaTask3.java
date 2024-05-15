cd30b5a6725b60652019824f31974b4b5d1fa288
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-05-15 18:15:46 +0300

    try to write JavaTask3.java

--- src/lesson02/JavaTask3.java
+++ src/lesson02/JavaTask3.java
@@ -1,5 +1,24 @@
 package lesson02;
 
+import java.util.Scanner;
+
 public class JavaTask3 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+
+    int [][] step1 (int n){
+
+        int [][] array = new int[n][n];
+        int max = n;
+        int min = -n;
+        for (int i = 0; i < array.length; i++) {
+            max = max - min;
+            n = (int) (Math.random()* ++max) + min;
+        }
+        return new int[n][n];
+    }
 }
