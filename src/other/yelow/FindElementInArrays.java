5e563603b1d65414aca7ec9dc6b12f402f17dd2a
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-06-05 09:46:46 +0300

    reformat yellow

--- src/other/yelow/FindElementInArrays.java
+++ src/other/yelow/FindElementInArrays.java
@@ -8,22 +8,14 @@ public class FindElementInArrays {
         // Даны три неубывающих массива чисел
         // Найти число, которое присутствует во всех трех массивах
 
-        int[] arr1 = new int[] {1,2,4,7,4,2,5,7,8,6};
-        int[] arr2 = new int[] {0,3,3,4,8,6,8,5,3,3};
-        int[] arr3 = new int[] {2,4,5,1,4,6};
+        int[] arr1 = new int[]{1, 2, 4, 7, 0, 2, -1, 8, 6, 9, 1};
+        int[] arr2 = new int[]{0, 3, -7, 4, 8, 6, 7, 5, 4, 9};
+        int[] arr3 = new int[]{2, 4, 5, 1, 7, 6};
 
-//        for (int i = 0; i < array3.length; i++) {
-//            if (array3[i] == array1[i] && array3[i] == array2[i]) {
-//                System.out.println("Элемент массива №" + i + " найден. Он равен " + array3[i]);
-//            } else System.out.println("Элемент массива №" + i + " не найден");
-//        }
-
-        // Отсортируем (просто по возрастанию) массивы для более эффективного поиска
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         Arrays.sort(arr3);
 
-        // Найдем общее число в трех массивах
         int i = 0;
         int j = 0;
         int k = 0;
