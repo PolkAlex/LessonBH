137667eb85e34310275a127dc0d8f52160d32995
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-06-10 14:45:02 +0300

    add maven framework support and some test HerokuApp

---
+++ src/test/java/HerokuAppTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HerokuAppTest {
+
+    @Test
+    public void test1() {
+        //БАЗОВЫЕ 5 СТРОК КОДА
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+}
