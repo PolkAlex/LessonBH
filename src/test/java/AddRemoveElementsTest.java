e8cbab51e4e1cda70530560567ccfbe989be3ff9
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-06-10 15:50:22 +0300

    refactor tests and add new tests

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -1,47 +1,25 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
-public class AddRemoveElementsTest {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
-
+public class AddRemoveElementsTest extends BaseTest {
 
     //добавить два элемента, проверить количество элементов
     //затем удалить один элемент, проверить количество элементов
     @Test
     public void elementCanBeAddedAndRemoved() {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        WebElement addButton =  driver.findElement(By.cssSelector("[onclick='addElement()']"));
+        WebElement addButton = driver.findElement(By.cssSelector("[onclick='addElement()']"));
         addButton.click();
         addButton.click();
-        List<WebElement> deleteButtons =  driver.findElements(By.cssSelector("[onclick='deleteElement()']"));
+        List<WebElement> deleteButtons = driver.findElements(By.cssSelector("[onclick='deleteElement()']"));
         assertEquals(deleteButtons.size(), 2, "Кол-во кнопок на странице не совпадает");
         deleteButtons.get(1).click();
         deleteButtons =  driver.findElements(By.cssSelector("[onclick='deleteElement()']"));
         assertEquals(deleteButtons.size(), 1, "Кол-во кнопок на странице не совпадает");
-    }
-
-    @AfterMethod (alwaysRun = true)
-    public void tearDown() {
-        driver.quit();
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,23 @@
-public class LoginTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class LoginTest extends BaseTest {
+
+    String userName = "tomsmith";
+    String password = "SuperSecretPassword!";
+    @Test
+    public void login() {
+
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys(userName);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("[type='submit']")).click();
+        String flashSuccessLogin = driver.findElement(By.cssSelector("[class='flash success']")).getText();
+        assertEquals(flashSuccessLogin,
+                "You logged into a secure area!\n" + "×",
+                "Не удалось залогиниться");
+    }
 }
