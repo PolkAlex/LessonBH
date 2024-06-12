63d24e9d8ff2f1dc0478e9732b9f87c6d9f2692b
PolkhovichAY <aleksandr.polkhovich@gmail.com>
2024-06-12 13:35:54 +0300

    add api tests

---
+++ src/test/java/api/AuthenticationTest.java
@@ -0,0 +1,20 @@
+package api;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class AuthenticationTest extends BaseTest {
+
+    @Test
+    public void successAuth() {
+        given().
+                auth().oauth2(getBearerToken()).
+        when().
+                get("https://api.stripe.com/v1/charges").
+        then().
+                statusCode(200).
+                body("url", equalTo("/v1/charges"));
+    }
+}
---
+++ src/test/java/api/BaseTest.java
@@ -0,0 +1,14 @@
+package api;
+
+public class BaseTest {
+
+    private String bearerToken = "sk_test_51PQnwEP2JhEktLmiiO38LlNO7J31nxfXu7jwAIo1AChzHF67Qj9Lvs1ixTX7Uc83ImcowTLpXCDBlZ7a48mHbIlt00lydmKoK8";
+
+    public String getBearerToken() {
+        return bearerToken;
+    }
+
+    public void setBearerToken(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+}
